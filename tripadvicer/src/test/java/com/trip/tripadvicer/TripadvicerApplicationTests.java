bb6aa13b6063451b3152fbfcd5b2af9259018b3d
Sanil <sanilgk@gmail.com>
2026-02-25 18:22:30 +0100

    commmited other files for Spring

---
+++ tripadvicer/src/test/java/com/trip/tripadvicer/TripadvicerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trip.tripadvicer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripadvicerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
